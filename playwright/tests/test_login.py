29ce1a84e238864f92beacb2f35239fd017d1f88
Arham <mominaj58@gmail.com>
2026-09-25 22:32:45 +0500

    Prepare Playwright tests for CI

--- playwright/tests/test_login.py
+++ playwright/tests/test_login.py
@@ -22,7 +22,7 @@ def test_open_login_page():
 
 def test_logout():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
 
         page = browser.new_page()
 
